5073ded9e240ffea4fea94fa62e4b7cf278c50c9
DilunVass <100837990+DilunVass@users.noreply.github.com>
2024-04-18 22:22:43 +0530

    API testing lecturer, lectureHall, labs

---
+++ AF_Assessment/src/test/java/com/AF_Assessment/AF_Assessment/api/API/LectureHallAPITests.java
@@ -0,0 +1,90 @@
+package com.AF_Assessment.AF_Assessment.api.API;
+
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@DisplayName("LectureHallAPITests")
+@Tag("Integration")
+public class LectureHallAPITests {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    private String token;
+
+    @Value("http://localhost:8080")
+    String baseUrl;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        String requestBody = "{\"username\": \"diun@gmail.com\", \"password\": \"abc\"}";
+        // Perform a POST request to obtain the token
+        this.token = mockMvc.perform(MockMvcRequestBuilders.post(this.baseUrl + "/token")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestBody))
+                .andExpect(status().isOk())
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+    }
+
+    @Test
+    void testFindAllLectureHallSuccess() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get(baseUrl + "/api/v1/lectureHalls")
+                        .header("Authorization", "Bearer " + token)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void testAddLectureHallSuccess() throws Exception {
+
+        String requestBody = "{\"type\": \"Computer Science\", \"hallCode\": \"A608\", \"description\": \"This is main building property}\"}";
+
+        mockMvc.perform(MockMvcRequestBuilders.post(baseUrl + "/api/v1/addlecturehall")
+                        .header("Authorization", "Bearer " + token)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .content(requestBody))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void testUpdateLectureHallSuccess() throws Exception {
+        String lectureHallId = "66211bb74622bf4d35cf219b";
+        String requestBody = "{\"type\": \"Computer Science\", \"hallCode\": \"A609\", \"description\": \"This is main building property}\"}";
+
+        mockMvc.perform(MockMvcRequestBuilders.put(baseUrl + "/api/v1/updateLectureHall/" + lectureHallId)
+                        .header("Authorization", "Bearer " + token)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestBody))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void testDeleteLectureHallSuccess() throws Exception {
+        String lectureHallId = "660003e4b223e57b5b7218ca";
+
+        // Perform DELETE request to delete the lecture hall
+        mockMvc.perform(MockMvcRequestBuilders.delete(baseUrl + "/api/v1/deleteLectureHall/" + lectureHallId)
+                        .header("Authorization", "Bearer " + token)
+                        .contentType(MediaType.APPLICATION_JSON))
+
+                .andExpect(status().isOk());
+    }
+
+}
---
+++ AF_Assessment/src/test/java/com/AF_Assessment/AF_Assessment/api/API/LecturerAPITests.java
@@ -0,0 +1,106 @@
+package com.AF_Assessment.AF_Assessment.api.API;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@DisplayName("LecturerAPITests")
+@Tag("Integration")
+public class LecturerAPITests {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    private String token;
+
+    @Value("http://localhost:8080")
+    String baseUrl;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        String requestBody = "{\"username\": \"diun@gmail.com\", \"password\": \"abc\"}";
+        // Perform a POST request to obtain the token
+        this.token = mockMvc.perform(MockMvcRequestBuilders.post(this.baseUrl + "/token")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestBody))
+                .andExpect(status().isOk())
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+    }
+
+    @Test
+    void testFindAllLecturerSuccess() throws Exception {
+        // Perform GET request to the API endpoint
+        mockMvc.perform(MockMvcRequestBuilders.get(baseUrl + "/api/v1/lecturers")
+                        .header("Authorization", "Bearer " + token)
+                        .accept(MediaType.APPLICATION_JSON))
+
+                .andExpect(status().isOk())
+
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+
+                .andExpect(jsonPath("$").isArray())
+
+                .andExpect(jsonPath("$[0]").exists())
+
+                .andExpect(jsonPath("$[0].firstName").value("dlun3"))
+                .andExpect(jsonPath("$[0].lastName").value("vss3"))
+                .andExpect(jsonPath("$[0].email").value("diun4@gmail.com"))
+                .andExpect(jsonPath("$[0].type").value("visiting"));
+    }
+
+    @Test
+    void testAddLecturerSuccess() throws Exception {
+        // Define the request body for adding a new lecturer
+        String requestBody = "{\"firstName\": \"John\", \"lastName\": \"Doe\", \"email\": \"johndoe2@example.com\", \"type\": \"permanent\"}";
+
+
+        mockMvc.perform(MockMvcRequestBuilders.post(baseUrl + "/api/v1/addlecturer")
+                        .header("Authorization", "Bearer " + token)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestBody))
+
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void testDeleteLecturerSuccess() throws Exception {
+
+        String lecturerId = "65fd783592ea3f1a0cef2aee";
+
+        // Perform DELETE request to delete the lecturer
+        mockMvc.perform(MockMvcRequestBuilders.delete(baseUrl + "/api/v1/lecturerDelete/" + lecturerId)
+                        .header("Authorization", "Bearer " + token)
+                        .contentType(MediaType.APPLICATION_JSON))
+
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void testUpdateLecturerSuccess() throws Exception {
+
+        String lecturerId = "6620d12740c473144ee00a02";
+        String requestBody = "{\"firstName\": \"dlunvass\", \"lastName\": \"vss3\", \"email\": \"diun04@gmail.com\", \"password\": \"abc\", \"type\": \"Visiting\"}";
+
+        mockMvc.perform(MockMvcRequestBuilders.put(baseUrl + "/api/v1/updateLecturer/" + lecturerId)
+                        .header("Authorization", "Bearer " + token)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestBody))
+                .andExpect(status().isOk());
+    }
+
+}
+
